83414334a85d20224edb2d33f079924c132f504a
asu <sdelang@sdelang.fr>
2023-10-24 14:27:59 +0200

    Missing copyright notice in noterender

--- src/gui/noterender.cpp
+++ src/gui/noterender.cpp
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2022 sdelang
+
 #include "SFML/Graphics/BlendMode.hpp"
 #include "SFML/Graphics/PrimitiveType.hpp"
 #include "SFML/Graphics/Transform.hpp"
